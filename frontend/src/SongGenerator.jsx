028cb9e2c7723c39044d93ebee74a2c3e10aaf24
namlee1998 <longtaiew01@gmail.com>
2025-08-12 15:32:32 +0700

    Update SongGenerator.jsx

--- frontend/src/SongGenerator.jsx
+++ frontend/src/SongGenerator.jsx
@@ -8,7 +8,7 @@ export default function SongGenerator() {
 
   const generateSong = async (prompt) => {
     setLoading(true);
-    const res = await fetch("http://0.0.0.0:8080/generate", {
+    const res = await fetch("https://genmusic-486965435366.asia-southeast1.run.app/generate", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ prompt }),
