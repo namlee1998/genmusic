b2257589380c3a2d328055c5fa936d13f809b714
namlee1998 <longtaiew01@gmail.com>
2025-08-18 09:46:02 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-from backend.aimusic import MusicGenerator  # 👈 Class đã đóng gói xử lý
+from aimusic import MusicGenerator  # 👈 Class đã đóng gói xử lý
 
 # ==============================
 # Logger setup
